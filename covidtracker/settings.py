07284ed3931da81db9e8723b2f857a09eab6f220
Mamtachahal <ankitchahal2812.com>
2022-07-12 16:42:02 +0530

    hi

--- covidtracker/settings.py
+++ covidtracker/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'covid'
+    'covid',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +126,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = ([
+#     os.path.join(os.path.dirname(BASE_DIR), 'staticfiles'),    
+# ])
+
+
+STATIC_ROOT = BASE_DIR / 'static'
